7986d5f7578a1e61b825beccb886855d2d5b6980
kiran <pskiran90@gmail.com>
2023-02-28 15:11:14 +0530

    avocadoo

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:avocado/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/auth_provider.dart';
+import 'screens/splash_screen.dart';
+import 'utils/avocado_theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: avocadoTheme,
+        debugShowCheckedModeBanner: false,
+        home: const AvocadoSplashScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/otp_verfication.dart
@@ -0,0 +1,139 @@
+import 'package:avocado/providers/auth_provider.dart';
+import 'package:avocado/screens/profile_page.dart';
+import 'package:avocado/utils/colors.dart';
+import 'package:avocado/widgets/custome_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pinput/pinput.dart';
+import 'package:provider/provider.dart';
+
+import '../utils/utils.dart';
+import 'home_screen.dart';
+
+class OtpScreen extends StatefulWidget {
+  final String verificationId;
+  const OtpScreen({super.key, required this.verificationId});
+
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  String? otpCode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: const Icon(Icons.arrow_back),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Verification",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Enter the OTP send to your phone number",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                //otp
+                Pinput(
+                  length: 6,
+                  showCursor: true,
+                  defaultPinTheme: PinTheme(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: kAccentColor,
+                      ),
+                    ),
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  onCompleted: (value) {
+                    setState(() {
+                      otpCode = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 25),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: AvocadoButton(
+                    text: "Verify",
+                    onPressed: () {
+                      if (otpCode != null) {
+                        verifyOtp(context, otpCode!);
+                      } else {
+                        showSnackBar(context, "Enter 6-Digit code");
+                      }
+                    },
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Didn't receive any code?",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black38,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  "Resend New Code",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void verifyOtp(BuildContext context, String userOtp) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+    ap.verifyOtp(
+        context: context,
+        verificationId: widget.verificationId,
+        userOtp: userOtp,
+        onSuccess: () {
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomeScreen(),
+              ),
+              (route) => false);
+        });
+  }
+}
